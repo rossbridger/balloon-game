ef929184822b1b69c1a9102358e35e18536facd7
Ross Charles Campbell <10781734+rossbridger@users.noreply.github.com>
2024-09-02 20:17:39 +0700

    Add health bar to the balloon.
    Signed-off-by: Ross Charles Campbell <10781734+rossbridger@users.noreply.github.com>

--- balloon.gd
+++ balloon.gd
@@ -4,15 +4,20 @@ extends Node3D
 @export var clicks_to_pop: int = 5
 @export var scale_increase_per_click: float = 0.1
 
+var clicks: int
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	$SubViewport/HealthBar.value = 100
+	clicks = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
 func _on_mouse_down():
-	clicks_to_pop -= 1
+	clicks += 1
+	var health = 100 * float(clicks_to_pop - clicks) / clicks_to_pop
+	$SubViewport/HealthBar.value = health
 	scale += Vector3.ONE * scale_increase_per_click
+	
